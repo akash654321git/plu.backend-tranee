2dd0c07dc4f6398013153f1d0fcdfad9c6455087
akash654321git <106536294+akash654321git@users.noreply.github.com>
2022-08-17 04:37:35 -0700

    mongodbsession2book

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,16 +4,28 @@ const router = express.Router();
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", BookController.createBook )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/bookList", BookController.allBooksList)
 
-router.post("/createBook", BookController.createBook  )
+router.post("/yearDetails", BookController.yearDetails)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.post("/particularBooks", BookController.particularBooks)
+
+router.get("/priceDetails", BookController.priceDetails  )
+
+router.get("/randomBooks", BookController.randomBooks)
+
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createBook", BookController.createBook  )
+
+// router.get("/getBooksData", BookController.getBooksData)
 
 module.exports = router;
